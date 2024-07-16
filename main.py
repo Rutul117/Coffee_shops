92f46fbcba4ac52c811683ee5308cecbf87e9dbe
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2024-07-16 19:40:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
+from werkzeug.urls import urlencode
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
